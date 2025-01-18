33a21fba6d8f84af90eae496a3c998a6578a686a
NikhilDongre1 <nikhildongre8109.com>
2025-01-18 11:19:04 +0530

    added them switch

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,18 +3,22 @@ import React from 'react'
 import MenuItem from './MenuItem'
 import { AiFillHome } from 'react-icons/ai';
 import { BsFillInfoCircleFill } from 'react-icons/bs';
+import DarkModeSwither from './DarkModeSwither';
 
 const Header = () => {
   return (
     <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
       <div className='flex gap-4 '>
-       <MenuItem title='home' address='/' Icon={AiFillHome} />
-       <MenuItem title='about' address='/about' Icon={BsFillInfoCircleFill} />
+        <MenuItem title='home' address='/' Icon={AiFillHome} />
+        <MenuItem title='about' address='/about' Icon={BsFillInfoCircleFill} />
       </div>
+      <div className='flex gap-2 items-center'>
+      <DarkModeSwither /> 
       <Link href='/' className='flex items-center gap-1'>
         <span className='text-l font-bold bg-amber-500 p-1 rounded'>IMDB</span> <span className='text-l hidden sm:inline'>Clone</span>
       </Link>
-     
+      </div>
+
     </div>
   )
 }
